c33e81888916f73d2568bac32642b72577d22a0d
Code-Monster1 <usupovaliakbar01@gmail.com>
2024-03-06 22:30:00 +0600

    add without design component

--- src/components/WithoutDesign/WithoutDesign.tsx
+++ src/components/WithoutDesign/WithoutDesign.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import styles from "./WithoutDesign.module.scss";
+
 const WithoutDesign = () => {
-  return <div>WithoutDesign</div>;
+  return (
+    <section className={styles.withoutDesign}>
+      <h1 className={styles.withoutDesign__title}>
+        У страницы не было дизайна
+      </h1>
+      <div className={styles.withoutDesign__content}>
+        <p className={styles.withoutDesign__desc}>
+          Разработчик Akloom приносит свои извинения, но на данной странице не
+          было дизайна, поэтому она не сделана
+        </p>
+        <Link href="/" className={styles.withoutDesign__link}>
+          Вернуться на главную
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default WithoutDesign;
